501544998a07993216da9e5c8471d7cb3e3433da
Akash-Kamaliya <akashkamaliya112@gmail.com>
2025-09-02 11:06:49 +0530

    make a new repo to save my academic project

---
+++ SkyCar/my-react-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './header';
+import Main from './main';
+import Footer from './footer';
+function App() {
+  return (
+    <>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
